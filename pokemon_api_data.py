4bbb6de0d7b9290ae2f15482fae52118199f0284
Hari Sapkota <hari.sapkota1@gmail.com>
2026-04-20 16:40:19 +0100

    update pokemon_api_data.py

--- pokemon_api_data.py
+++ pokemon_api_data.py
@@ -16,25 +16,22 @@ Try and work collaboratively on the one repo using Git
 
 import requests
 import json
-from pprint import pprint
 
 # Get the list of pokemon from the API
 url = 'https://pokeapi.co/api/v2/pokemon/'
 response = requests.get(url)
 response_json = response.json()
-# print("\njson:", response_json)
 
 results = response_json["results"]
-print("\nresults: ", results)
 
 pokemon_list = []
 for pokemon in results:
     pokemon_list.append(pokemon["name"])
 
-print(pokemon_list)
+print("\npokemon_list: ", pokemon_list)
 
 # Ask the user to choose a pokemon
-print('Enter your pokemon:')
+print('\nEnter your pokemon:')
 
 # Get the user's choice
 choice = input().lower()
@@ -43,30 +40,49 @@ choice = input().lower()
 url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(choice)
 response = requests.get(url)
 pokemon_data = json.loads(response.text)
-print("\npokemon_data", pokemon_data)
+# print("\npokemon_data", pokemon_data)
 
+keys = []
+for k, v in pokemon_data.items():
+    keys.append(k)
 
-'''
-name, hp, attack, defense, speed, power
-'''
+# print("\nkeys: ", keys)
+
 # to get ability
 abilities = pokemon_data['abilities'][0]
-print("\nabilities: ", abilities)
-ability = abilities['ability']
-print("\nability: ", ability)
+# print("\nabilities: ", abilities)
+ability_name = abilities['ability']['name']
+# print("\nability: ", ability_name)
 
 # to format height and weight properly
 height = int(pokemon_data['height'])
-attack = pokemon_data['attack']
-print("\nattack: ", attack)
+# attack = pokemon_data['attack']
+# print("\nattack: ", attack)
 weight = int(pokemon_data['weight'])
 
 height_formatted = height /10
 weight_formatted = weight /10
 
+'''
+get the following from the api:
+hp, attack, defense, speed
+'''
+
+stats = pokemon_data["stats"]
+# print("\nstats: ", stats)
+
+hp = stats[0]["base_stat"]
+attack = stats[1]["base_stat"]
+defense = stats[2]["base_stat"]
+speed = stats[5]["base_stat"]
+
 # Print the pokemon's data
+print("\n")
 print('Name: {}'.format(pokemon_data['name']))
 print('Weight: {}'.format(weight_formatted) +"(kgs)")
 print('Height: {}'.format(height_formatted) +"(m)")
-print('Ability: {}'.format(ability['name']))
-
+print('Ability: {}'.format(ability_name))
+print("HP: ", hp)
+print("Attack: ", attack)
+print("Defense: ", defense)
+print("Speed: ", speed)
